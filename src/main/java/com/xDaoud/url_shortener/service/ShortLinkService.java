067c339ecf7698638d9a137249fca9eab91653a6
xDaoud <moshdawd@gmail.com>
2025-11-06 21:47:18 +0200

    feat: add createShortLink method with hash generation and collision handling

--- src/main/java/com/xDaoud/url_shortener/service/ShortLinkService.java
+++ src/main/java/com/xDaoud/url_shortener/service/ShortLinkService.java
@@ -4,6 +4,10 @@ import com.xDaoud.url_shortener.model.ShortLink;
 import com.xDaoud.url_shortener.repository.ShortLinkRepository;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 @Service
 public class ShortLinkService {
     ShortLinkRepository shortLinkRepository;
@@ -19,5 +23,31 @@ public class ShortLinkService {
         else {
             return isFound.getUrl();
         }
+    }
+
+    public ShortLink createShortLink(String originalUrl) {
+        String baseHash = generateHash(originalUrl);
+        String finalHash = findUniqueHash(baseHash);
+        ShortLink shortLink = new ShortLink(originalUrl, finalHash);
+        shortLinkRepository.save(shortLink);
+        return shortLink;
+    }
+    private String generateHash(String originalUrl) {
+        String input = originalUrl + System.nanoTime();
+        try {
+            byte[] hash = MessageDigest.getInstance("MD5").digest(input.getBytes());
+            return Base64.getUrlEncoder().encodeToString(hash).substring(0, 8);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private String findUniqueHash(String baseHash) {
+        String tempHash = baseHash;
+        int attempts = 0;
+        while(shortLinkRepository.findByHash(tempHash) != null) {
+            tempHash = baseHash + "-" + attempts;
+            attempts++;
+        }
+        return tempHash;
     }
 }
